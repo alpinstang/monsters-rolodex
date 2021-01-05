c3c47186e902685ee83af0ab8c1205bf87647979
John McDonald <jmcdonald1991@gmail.com>
2021-01-05 00:23:54 -0600

    monster map list

--- src/App.js
+++ src/App.js
@@ -7,18 +7,34 @@ constructor() {
   super();
 
   this.state = {
-    string: 'Hello John'
+    monsters: [
+      {
+        name: 'Bob',
+        id: 'asc1'
+      },
+      {
+        name: 'Frankenstein',
+        id: 'asc2'
+      },
+      {
+        name: 'Dracula',
+        id: 'asc3'
+      },
+      {
+        name: 'Dr. Jekyll',
+        id: 'asc4'
+      }
+    ]
   }
 }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.string}</p>
+          {this.state.monsters.map(monster => (
+            <h1 key={monster.id}> { monster.name} </h1>)
+          )}
           <button onClick={() => this.setState({ string: 'Hey again John'})}>Change Text</button>
-        </header>
       </div>
     );
   }  
