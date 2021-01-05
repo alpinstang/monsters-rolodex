00c7f2c582a355eb62eb68ae9f395930102c3f5e
John McDonald <jmcdonald1991@gmail.com>
2021-01-05 00:46:25 -0600

    sample api fetch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,34 +6,22 @@ constructor() {
   super();
 
   this.state = {
-    monsters: [
-      {
-        name: 'Bob',
-        id: 'asc1'
-      },
-      {
-        name: 'Frankenstein',
-        id: 'asc2'
-      },
-      {
-        name: 'Dracula',
-        id: 'asc3'
-      },
-      {
-        name: 'Dr. Jekyll',
-        id: 'asc4'
-      }
-    ]
-  }
+    monsters: [{}]
+  };
+}
+
+componentDidMount() {
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then( response => response.json())
+  .then(users =>  this.setState({monsters: users}))
 }
 
   render() {
     return (
       <div className="App">
           {this.state.monsters.map(monster => (
-            <h1 key={monster.id}> { monster.name} </h1>)
-          )}
-          <button onClick={() => this.setState({ string: 'Hey again John'})}>Change Text</button>
+            <h1 key={monster.id}> {monster.name} </h1>
+          ))}
       </div>
     );
   }  
